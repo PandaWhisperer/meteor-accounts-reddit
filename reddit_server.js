93f0eecced8a020d3553c6ff2b7addb8eee795f8
Christoph Wagner <chwagner@gmail.com>
2014-07-09 23:17:14 -0700

    Removing unused autopublish fields

--- reddit_server.js
+++ reddit_server.js
@@ -3,7 +3,5 @@ Accounts.addAutopublishFields({
   // be used from the client (if transmitted over ssl or on
   // localhost). https://developer.linkedin.com/documents/authentication
   forLoggedInUser: ['services.reddit'],
-  forOtherUsers: [
-    'services.reddit.id', 'services.reddit.firstName', 'services.reddit.lastName'
-  ]
+  forOtherUsers: ['services.reddit.id']
 });
