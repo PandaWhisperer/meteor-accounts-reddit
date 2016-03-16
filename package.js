344ccf967e2208cd45d975c4cf1f9d0fffd2d12a
Christoph Wagner <github@pandawhisperer.net>
2016-03-15 23:56:44 -0700

    Bumping version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pandawhisperer:accounts-reddit',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'Accounts service for reddit',
   // URL to the Git repository containing the source code for this package.
